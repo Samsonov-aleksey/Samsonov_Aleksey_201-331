fd37a0740e0ba1c4406cffbfad81ae36c9fae000
Samsonov-aleksey <71924957+Samsonov-aleksey@users.noreply.github.com>
2020-12-18 22:04:47 +0300

    lab6.1

---
+++ Samsonov_Aleksey_201-331_lab6_1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+	string a;
+	cin >>a;
+	stack<char> s;
+	for (int i = 0; i <a.length(); i++) {
+		if (a[i] == '{' or a[i] == '(' or a[i] == '[') {
+			s.push(a[i]);
+		}
+		else if ((a[i] == '}' and s.top() == '{') or (a[i] == ']' and s.top() == '[') or (a[i] == ')' and s.top() == '(')) {
+			s.pop();
+		}
+	}
+	if (s.empty()) {
+		cout << "YES" << endl;
+	}
+	else {
+		cout << "NO" << endl;
+	}
+	return 0;
+}
