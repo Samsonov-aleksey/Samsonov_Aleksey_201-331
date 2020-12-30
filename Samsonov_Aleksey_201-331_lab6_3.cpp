26c00033a26b4b9b3792fd3b0981a28da35e4cce
Samsonov-aleksey <71924957+Samsonov-aleksey@users.noreply.github.com>
2020-12-30 16:37:22 +0300

    lab6.3

---
+++ Samsonov_Aleksey_201-331_lab6_3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <list>
+#include <map>
+using namespace std;
+int main(){
+	map<int, list<string>> m;
+	int n;
+	int number;
+	string surname;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> number >> surname;
+		m[number].push_back(surname);
+	}
+	for (pair<int,list<string>> item : m) {// проходимся по каждому элементу словаря
+		cout << item.first << ": ";
+		for (string name : item.second) {
+			cout << name << " ";
+		}
+		cout << endl;
+	}
+
+
+	return 0;
+ }
