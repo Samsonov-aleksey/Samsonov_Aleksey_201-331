0d748736f959f7328a4938ed160e9cde806d0a3b
Samsonov-aleksey <71924957+Samsonov-aleksey@users.noreply.github.com>
2020-12-30 17:57:09 +0300

    lab6.4

---
+++ Samsonov_Aleksey_201-331_lab6_4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <unordered_set>
+#include <string>
+using namespace std;
+
+int main() {
+	int n;
+	int id;
+	string answer;
+	cin >> n;
+	unordered_set<int> u;
+	for (int i = 0; i < n; i++) {
+		cin >> id >> answer;
+		if (u.count(id) != 1) {
+			u.insert(id);
+			cout << "The answer is counted" << endl;
+		}
+		else {
+			cout << "The answer isn't counted" << endl;
+		}
+	}
+
+	return 0;
+}
