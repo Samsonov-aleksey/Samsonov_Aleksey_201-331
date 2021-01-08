7660bbcaf075f2c6462b232532290516351ec21c
Samsonov-aleksey <71924957+Samsonov-aleksey@users.noreply.github.com>
2021-01-08 15:15:47 +0300

    lab6.6

---
+++ Samsonov_Aleksey_201-331_lab6_6.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+	map<string, int> m;
+	int count = 0;
+	string word;
+	while (cin >> word and word != "!") {
+		m[word]++;
+		count++;
+	}
+	for (pair<string, int> item : m) {
+		cout << item.first << ": " << double (item.second) / count *100 << endl;
+	}
+
+	return 0;
+}
